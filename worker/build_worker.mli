45a767dda09ba00732413c53d407c320283598b1
Thomas Leonard <talex5@gmail.com>
2020-06-24 10:18:54 +0100

    Require workers to provide an ID
    This will be needed so that the scheduler can remember which builders
    have cached which layers.

--- worker/build_worker.mli
+++ worker/build_worker.mli
@@ -5,9 +5,11 @@ val run :
                  src:string ->
                  string -> (unit, Process.error) Lwt_result.t) ->
   capacity:int ->
+  name:string ->
   Api.Raw.Client.Registration.t Capnp_rpc_lwt.Sturdy_ref.t ->
   unit Lwt.t
-(** [run ~capacity registry] runs a builder that connects to registry and runs up to [capacity] jobs at once.
+(** [run ~capacity ~name registry] runs a builder that connects to registry and runs up to [capacity] jobs at once.
+    The builder registers using the unique ID [name].
     @param switch Turning this off causes the builder to exit (for unit-tests)
     @param docker_build Used to override the default build action (for unit-tests) *)
 
