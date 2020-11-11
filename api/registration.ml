0eb1154f6bf80822c514d67c314a33f7ad720344
Thomas Leonard <talex5@gmail.com>
2020-11-11 16:39:33 +0000

    Workers report capacity when registering
    This is useful information about the cluster, and may also be useful
    for fair scheduling to decide how long we expect some work to take. It
    may also be useful if we want to reserve some capacity for urgent work.

--- api/registration.ml
+++ api/registration.ml
@@ -8,13 +8,18 @@ let local ~register =
     method register_impl params release_param_caps =
       let open X.Register in
       let name = Params.name_get params in
+      let capacity =
+        let x = Params.capacity_get_int_exn params in
+        if x > 0 then x
+        else 32   (* Old workers don't report their capacity. *)
+      in
       let worker = Params.worker_get params in
       release_param_caps ();
       match worker with
       | None -> Service.fail "Missing worker argument!"
       | Some worker ->
         let response, results = Service.Response.create Results.init_pointer in
-        let queue = register ~name worker in
+        let queue = register ~name ~capacity worker in
         Results.queue_set results (Some queue);
         Capability.dec_ref queue;
         Service.return response
@@ -24,9 +29,10 @@ module X = Raw.Client.Registration
 
 type t = X.t Capability.t
 
-let register t ~name worker =
+let register t ~name ~capacity worker =
   let open X.Register in
   let request, params = Capability.Request.create Params.init_pointer in
   Params.name_set params name;
   Params.worker_set params (Some worker);
+  Params.capacity_set_int_exn params capacity;
   Capability.call_for_caps t method_id request Results.queue_get_pipelined
