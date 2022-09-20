8fe020e6d2d6bd5081bef67d8c6161e4fa99ff6b
Mark Elvers <mark.elvers@tunbury.org>
2022-09-20 16:17:06 +0000

    Added free inodes check to free_space_percent

--- worker/df.ml
+++ worker/df.ml
@@ -7,5 +7,8 @@ let free_space_percent root_dir =
       root_dir
   in
   let vfs = ExtUnix.All.statvfs root_dir in
-  let used = Int64.sub vfs.f_blocks vfs.f_bfree in
-  100. -. 100. *. (Int64.to_float used) /. (Int64.to_float (Int64.add used vfs.f_bavail))
+  let used_blocks = Int64.sub vfs.f_blocks vfs.f_bfree in
+  let used_files = Int64.sub vfs.f_files vfs.f_ffree in
+  let percent_free_blocks = 100. -. 100. *. (Int64.to_float used_blocks) /. (Int64.to_float (Int64.add used_blocks vfs.f_bavail)) in
+  let percent_free_files = 100. -. 100. *. (Int64.to_float used_files) /. (Int64.to_float (Int64.add used_files vfs.f_favail)) in
+  min percent_free_blocks percent_free_files
