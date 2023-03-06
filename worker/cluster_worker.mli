17b2a93a9bc5ad4f4b00f85ff1b54b547ca2f184
Antonin Décimo <antonin@tarides.com>
2023-03-06 17:41:09 +0100

    Improve Cluster_worker.run doc

--- worker/cluster_worker.mli
+++ worker/cluster_worker.mli
@@ -37,19 +37,29 @@ val run :
   state_dir:string ->
   Cluster_api.Raw.Client.Registration.t Capnp_rpc_lwt.Sturdy_ref.t ->
   unit Lwt.t
-(** [run ~capacity ~name ~state_dir registry] runs a builder that connects to registry and runs up to [capacity] jobs at once.
-    The builder registers using the unique ID [name].
-    @param switch Turning this off causes the builder to exit (for unit-tests)
-    @param build Used to override the default build action (for unit-tests or custom job specifications)
-    @param allow_push Docker repositories to which results can be pushed
-    @param update Function to run on "selfUpdate" requests. It should do any preparation (such as downloading new images),
-                  then return a function to do the actual update. This is so that the first part can run while the node
-                  finishes its remaining jobs. The second part is called once all jobs are finished.
-                  If the second function returns, the process will exit.
-    @param state_dir A persistent directory for Git caches, etc.
-    @param prune_threshold Stop and run "docker system prune -af" if free-space is less than this percentage (0 to 100).
-    @param obuilder_prune_threshold The threshold for OBuilder to prune the store of cached build steps.
-    @param additional_metrics A list of additional prometheus endpoints with a descriptive name to collect. *)
+(** [run ~capacity ~name ~state_dir registry] runs a builder that connects to registry and runs up
+    to [capacity] jobs at once. The builder registers using the unique ID [name].
+    @param switch Turning this off causes the builder to exit (for unit-tests).
+    @param build Used to override the default build action (for unit-tests or custom job
+      specifications).
+    @param allow_push Docker repositories to which results can be pushed.
+    @param healthcheck_period Time period, in seconds, at which the health of the worker is checked.
+    @param prune_threshold Stop and run [docker system prune -af] if free-space is less than this
+      percentage (0 to 100).
+    @param docker_max_df_size Run [docker system df] to get the amount of memory being taken up by
+      the images and if this is greater than [docker_max_df_size], run [docker system prune].
+    @param obuilder_prune_threshold The threshold for OBuilder to prune the store of cached build
+      steps.
+    @param obuilder OBuilder's configuration.
+    @param additional_metrics A list of additional prometheus endpoints with a descriptive name to
+      collect.
+    @param update Function to run on "selfUpdate" requests. It should do any preparation (such as
+      downloading new images), then return a function to do the actual update. This is so that the
+      first part can run while the node finishes its remaining jobs. The second part is called once
+      all jobs are finished.  If the second function returns, the process will exit.
+    @param capacity The number of builds that can run in parallel.
+    @param name Unique builder name.
+    @param state_dir A persistent directory for Git caches, etc. *)
 
 module Process = Process
 module Log_data = Log_data
