4fbbbdf3186be355a2545bb34960745dfe875290
Antonin Décimo <antonin@tarides.com>
2023-06-02 18:31:20 +0200

    Print POSIX signal number (instead of OCaml) using Fmt (#231)
    * Print POSIX signal number (instead of OCaml) using Fmt
    
    * Use Fmt.error_msg in some cases

--- worker/process.ml
+++ worker/process.ml
@@ -13,13 +13,7 @@ let send_to ch contents =
        Lwt_io.close ch
     )
     (fun () -> Lwt.return (Ok ()))
-    (fun ex -> Lwt.return (Error (`Msg (Printexc.to_string ex))))
-
-let pp_signal f x =
-  let open Sys in
-  if x = sigkill then Fmt.string f "kill"
-  else if x = sigterm then Fmt.string f "term"
-  else Fmt.int f x
+    (fun ex -> Lwt.return (Fmt.error_msg "%a" Fmt.exn ex))
 
 let exec ~label ~log ~switch ?env ?(stdin="") ?(stderr=`FD_copy Unix.stdout) ?(is_success=((=) 0)) cmd =
   Log.info (fun f -> f "Exec(%s): %a" label Fmt.(list ~sep:sp (quote string)) cmd);
@@ -41,15 +35,15 @@ let exec ~label ~log ~switch ?env ?(stdin="") ?(stderr=`FD_copy Unix.stdout) ?(i
   | Unix.WEXITED n when is_success n ->
     begin match stdin_result with
       | Ok () -> Ok ()
-      | Error (`Msg msg) -> Error (`Msg (Fmt.str "Failed sending input to %s: %s" label msg))
+      | Error (`Msg msg) -> Fmt.error_msg "Failed sending input to %s: %s" label msg
     end
   | Unix.WEXITED n -> Error (`Exit_code n)
-  | Unix.WSIGNALED x -> Error (`Msg (Fmt.str "%s failed with signal %d" label x))
-  | Unix.WSTOPPED x -> Error (`Msg (Fmt.str "%s stopped with signal %a" label pp_signal x))
+  | Unix.WSIGNALED x -> Fmt.error_msg "%s failed with signal %a" label Fmt.Dump.signal x
+  | Unix.WSTOPPED x -> Fmt.error_msg "%s stopped with signal %a" label Fmt.Dump.signal x
 
 let check_call ~label ~log ~switch ?env ?stdin ?stderr ?is_success cmd =
   exec ~label ~log ~switch ?env ?stdin ?stderr ?is_success cmd >|= function
   | Ok () -> Ok ()
   | Error `Cancelled -> Error `Cancelled
-  | Error (`Exit_code n) -> Error (`Msg (Fmt.str "%s failed with exit-code %d" label n))
+  | Error (`Exit_code n) -> Fmt.error_msg "%s failed with exit-code %d" label n
   | Error (`Msg _) as e -> e
